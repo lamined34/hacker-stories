53ef7a6dcce3b0051e45e84526e1d9a3e47514d4
lamine <lamined34@gmail.com>
2024-10-08 22:08:38 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -32,39 +33,44 @@ const App = () => {
 
       <hr />
 
-      <List />
+      <List list={stories}/>
     </div>
   );
 }
 
 const Search = () => {
+  const handleChange = (event) => {
+    console.log(event)
+    console.log(event.target.value)
+  }
+
   return(
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange}/>
     </div>
   )
 }
 
-const List = () => {
-  return(
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title} </a>
-            </span>
-            <span> {item.author} </span>
-            <span> {item.num_comments} </span>
-            <span> {item.points} </span>
+const List = (props) =>  (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item}/>
+    ))}
+  </ul>
+)
 
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title} </a>
+    </span>
+    <span> {props.item.author} </span>
+    <span> {props.item.num_comments} </span>
+    <span> {props.item.points} </span>
+
+  </li>
+)
 
 
 export default App
