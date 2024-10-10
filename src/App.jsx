edbee32f5fe10d05875ec7489c9f551c4921691d
lamine <lamined34@gmail.com>
2024-10-10 15:48:33 +0000

    Nested destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react'
 
 
 
@@ -25,52 +26,107 @@ const App = () => {
     }
   ]
 
+  const [searchTerm, setSearchTerm] = React.useState('React')
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch}/>
 
       <hr />
 
-      <List list={stories}/>
+      <List list={searchedStories}/>
+
+      <Test />
     </div>
   );
 }
 
-const Search = () => {
-  const handleChange = (event) => {
-    console.log(event)
-    console.log(event.target.value)
-  }
-
+const Search = ({search, onSearch}) => {
   return(
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" onChange={handleChange}/>
+      <input id="search" type="text" value={search} onChange={onSearch}/>
+
+      <p>
+        Searching for: <strong> { search} </strong>
+      </p>
     </div>
   )
 }
 
-const List = (props) =>  (
+const List = ({list}) =>  (
   <ul>
-    {props.list.map((item) => (
+    {list.map((item) => (
       <Item key={item.objectID} item={item}/>
     ))}
   </ul>
 )
 
-const Item = (props) => (
+const Item = ({item}) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title} </a>
+      <a href={item.url}>{item.title} </a>
     </span>
-    <span> {props.item.author} </span>
-    <span> {props.item.num_comments} </span>
-    <span> {props.item.points} </span>
+    <span> {item.author} </span>
+    <span> {item.num_comments} </span>
+    <span> {item.points} </span>
 
   </li>
 )
 
+//To do delete
+const Test = () => {
+  const user = {
+    firstName : 'Robin',
+    lastName: 'Weiruch'
+  }
+
+  //Without destructuring
+  //const firstName = user.firstName
+  //const lastName = user.lastName
+
+  //With destructuring
+  const {firstName, lastName} = user
+
+  console.log(firstName, lastName)
+
+  /**Nested destructuring */
+  const user2 = {
+    firstName3: 'Biro',
+    pet: {
+      name3: 'Fidel'
+    }
+  }
+
+  //without nested destruturing
+  const firstName2 = user2.firstName3
+  const name2 = user2.pet.name3
+
+  console.log('######################################')
+  console.log(firstName2 + ' has a pet called ' +name2)
+
+  //With nested destructuring
+  const {
+    firstName3,
+    pet: {
+      name3,
+    },
+  } = user2
+
+  console.log('######################################')
+  console.log(firstName3 + ' has a pet called ' + name3)
+
+  
+}
 
 export default App
